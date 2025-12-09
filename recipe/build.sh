4b4d3e2bdb8ca2fa4f4b95d2c2a3c6ca5ba6751f
xumi1993 <gomijianxu@gmail.com>
2025-12-09 10:33:25 -0500

    MNT: Add MPI wrapper exports for Linux in build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,14 @@ mkdir build && cd build
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 
+# Use MPI wrappers
+if [ "$(uname)" == "Linux" ]; then
+    export OMPI_CXX="${CXX}"
+    export OMPI_CC="${CC}"
+    export CXX=mpicxx
+    export CC=mpicc
+fi
+
 cmake ${CMAKE_ARGS} -D CMAKE_INSTALL_PREFIX=$PREFIX ..
 make -j$CPU_COUNT
 make install
