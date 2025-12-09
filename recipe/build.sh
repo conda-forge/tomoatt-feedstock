bc8ec2e960dd5e9df4ff1bf89327a0e6f6a0a245
xumi1993 <gomijianxu@gmail.com>
2025-12-09 10:52:02 -0500

    MNT: Refactor MPI compiler variable setup in build script for clarity

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,15 +7,21 @@ mkdir build && cd build
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 
-# Use MPI wrappers
+# Set MPI compiler variables for CMake
 if [ "$(uname)" == "Linux" ]; then
     export OMPI_CXX="${CXX}"
     export OMPI_CC="${CC}"
-    export CXX="$PREFIX/bin/mpicxx"
-    export CC="$PREFIX/bin/mpicc"
+    
+    # Use standard CMake variables for MPI
+    # We do NOT set CMAKE_CXX_COMPILER to mpicxx because we want to use the conda compiler
+    # Instead we let FindMPI use the wrappers to determine flags
+    export MPI_CXX_COMPILER="$PREFIX/bin/mpicxx"
+    export MPI_C_COMPILER="$PREFIX/bin/mpicc"
 fi
 
-cmake ${CMAKE_ARGS} -D CMAKE_INSTALL_PREFIX=$PREFIX ..
+cmake ${CMAKE_ARGS} \
+      -D CMAKE_INSTALL_PREFIX=$PREFIX \
+      ..
 make -j$CPU_COUNT
 make install
 
