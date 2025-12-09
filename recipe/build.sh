ea838e351ade36dc760bd6e8327a36660dbd184a
xumi1993 <gomijianxu@gmail.com>
2025-12-09 11:17:00 -0500

    MNT: Remove MPI compiler variable setup from build script for Linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,19 +7,10 @@ mkdir build && cd build
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 
-# Set MPI compiler variables for CMake
-if [ "$(uname)" == "Linux" ]; then
-    echo `ls $PREFIX/bin`
-    export OMPI_CXX="${CXX}"
-    export OMPI_CC="${CC}"
-    
-    # Use standard CMake variables for MPI
-    export MPI_CXX_COMPILER="$PREFIX/bin/mpicxx"
-    export MPI_C_COMPILER="$PREFIX/bin/mpicc"
-fi
 
 cmake ${CMAKE_ARGS} \
       -D CMAKE_INSTALL_PREFIX=$PREFIX \
+      -D CMAKE_PREFIX_PATH=$PREFIX \
       ..
 make -j$CPU_COUNT
 make install
