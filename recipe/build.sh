d99c990fc1621bacdfbd3cd5b87a16db0a1dcd9c
xumi1993 <gomijianxu@gmail.com>
2025-12-09 11:00:31 -0500

    MNT: Update build script to display contents of bin directory and clarify MPI compiler setup

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,13 +9,11 @@ export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
 
 # Set MPI compiler variables for CMake
 if [ "$(uname)" == "Linux" ]; then
-    echo `which mpicxx`
+    echo `ls $PREFIX/bin`
     export OMPI_CXX="${CXX}"
     export OMPI_CC="${CC}"
     
     # Use standard CMake variables for MPI
-    # We do NOT set CMAKE_CXX_COMPILER to mpicxx because we want to use the conda compiler
-    # Instead we let FindMPI use the wrappers to determine flags
     export MPI_CXX_COMPILER="$PREFIX/bin/mpicxx"
     export MPI_C_COMPILER="$PREFIX/bin/mpicc"
 fi
